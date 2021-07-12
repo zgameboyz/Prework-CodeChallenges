6f3ab2a2ad32b327be18baf7e8b54401bfcad5d1
zgameboy <joel.connell@yahoo.com>
2021-07-11 20:10:27 -0700

    Attempted to do problem 1

---
+++ CodingPrework/CodingPrework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodingPrework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            arrayMaxResult(array);
+
+
+            
+                   // I am having strange errors and I do not know what to do
+
+        }
+
+        public static int[] arrayMaxResult(int[] array)
+        {
+
+
+            int theArray[] = array;  
+
+            return ;
+        }
+    }
+}
